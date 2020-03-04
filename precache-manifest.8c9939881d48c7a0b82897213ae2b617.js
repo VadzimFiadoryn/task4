3f0b81be4e0863778ee69dab49472c38dde435ba
Vadzim <vadzimfiadoryn@gmail.com>
2020-03-04 13:22:19 +0300

    Updates

---
+++ precache-manifest.8c9939881d48c7a0b82897213ae2b617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537640cdd60cd9ee261b7f6c57a21df1",
+    "url": "/task4yarn/%20/index.html"
+  },
+  {
+    "revision": "6d265e7ac22b4025331c",
+    "url": "/task4yarn/%20/static/css/main.ebace1ae.chunk.css"
+  },
+  {
+    "revision": "5a832cc9e37304588ab0",
+    "url": "/task4yarn/%20/static/js/2.1342ff60.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/task4yarn/%20/static/js/2.1342ff60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d265e7ac22b4025331c",
+    "url": "/task4yarn/%20/static/js/main.cfb5fbee.chunk.js"
+  },
+  {
+    "revision": "ffa8d0bba0fe323008b6",
+    "url": "/task4yarn/%20/static/js/runtime-main.caf084d0.js"
+  }
+]);
