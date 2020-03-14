e1104ef3adaf258d4dcf2f36b7e0147f87497ec0
Vadzim <vadzimfiadoryn@gmail.com>
2020-03-14 13:28:59 +0300

    add res & save local Storage func

---
+++ src/SecondTask/surse/restoreState.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const restoreState = () => {
+    let state = {
+        tasks: [],
+        filterValue: "All"
+    }
+    let stateAsString = localStorage.getItem("our-state");
+    if (stateAsString != null) {
+        state = JSON.parse(stateAsString);
+    }
+    return state;
+}
+
+
+export default restoreState;
